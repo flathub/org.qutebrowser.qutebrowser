63a50ea000bc1f1606f8381f23a76daa1fdba4c0
tinywrkb <tinywrkb@gmail.com>
2022-02-24 21:39:11 +0200

    Update helper: correct modules' paths, and use subshells instead of pushd stack

--- update.sh
+++ update.sh
@@ -13,26 +13,28 @@ flatpak run \
   --edit-only ${_FPID}.yml
 
 for _mod in maturin python-adblock userscripts-dependencies/python-stem/python-cryptography:src/rust/Cargo.lock; do
-  pushd ${_mod%:*}
-  case $_mod in
-    *:*Cargo.lock)
-      ${_TOOLSDIR}/cargo-updater $(basename ${_mod%:*} ${_mod#*:})
-      ;;
-    *)
-      ${_TOOLSDIR}/cargo-updater $(basename ${_mod%:*})
-      ;;
-  esac
-  popd
+  (
+    cd ${_mod%:*}
+    case $_mod in
+      *:*Cargo.lock)
+        ${_TOOLSDIR}/cargo-updater $(basename ${_mod%:*} ${_mod#*:})
+        ;;
+      *)
+        ${_TOOLSDIR}/cargo-updater $(basename ${_mod%:*})
+        ;;
+    esac
+  )
 done
 
 # python modules with multiple dependencies and have a requirements.txt file
 for _mod in userscripts-dependencies/python-{beautifulsoup4,pocket-api,pykeepass,readability-lxml,stem/python-cryptography/python-setuptools-rust,tldextract}; do
-  pushd $_mod
-  ${_TOOLSDIR}/pip-updater $(basename $_mod)
-  popd
+  (
+    cd $_mod
+    ${_TOOLSDIR}/pip-updater $(basename $_mod)
+  )
 done
 
 # make only modules
-for _mod in asciidoc pyqt5/{pyqt-builder,pyqt5-sip,python-toml,sip} python-packaging-tools/python-{build,flit-core,installer,packaging,pip517,pyparsing,setuptools-scm,tomli} userscripts-dependencies/python-stem/python-cryptography/python-setuptools-rust{,python-{semantic-version,typing-extensions}}; do
+for _mod in asciidoc pyqt5/{pyqt-builder,pyqt5-sip,python-toml,sip} python-packaging-tools/python-{build,flit-core,installer,packaging,pep517,pyparsing,setuptools-scm,tomli} userscripts-dependencies/python-stem/python-cryptography/python-setuptools-rust{,/python-{semantic-version,typing-extensions}}; do
   ${_TOOLSDIR}/fedc-merge-updater $_mod
 done
