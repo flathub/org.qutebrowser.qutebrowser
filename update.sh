dd1206ba4476e3b5d79e0c3e9039168c369c57cf
tinywrkb <tinywrkb@gmail.com>
2022-02-24 21:18:08 +0200

    userscripts: Split python-setuptools-rust to seperate modules, use wheel source, disable fedc

--- update.sh
+++ update.sh
@@ -33,6 +33,6 @@ for _mod in userscripts-dependencies/python-{beautifulsoup4,pocket-api,pykeepass
 done
 
 # make only modules
-for _mod in asciidoc pyqt5/{pyqt-builder,pyqt5-sip,python-toml,sip} python-packaging-tools/python-{build,flit-core,installer,packaging,pip517,pyparsing,setuptools-scm,tomli}; do
+for _mod in asciidoc pyqt5/{pyqt-builder,pyqt5-sip,python-toml,sip} python-packaging-tools/python-{build,flit-core,installer,packaging,pip517,pyparsing,setuptools-scm,tomli} userscripts-dependencies/python-stem/python-cryptography/python-setuptools-rust{,python-{semantic-version,typing-extensions}}; do
   ${_TOOLSDIR}/fedc-merge-updater $_mod
 done
