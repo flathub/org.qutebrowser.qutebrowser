1ed42d7cd3ab7b025b017dca963a4462a50733ce
tinywrkb <tinywrkb@gmail.com>
2022-05-09 17:51:45 +0300

    update.sh: Remove userscripts-dependencies, disabled modules, reflect fedc.json merge changes

--- update.sh
+++ update.sh
@@ -8,7 +8,7 @@ _TOOLSDIR=$PWD/tools
 
 flatpak-external-data-checker --edit-only ${_FPID}.yml
 
-for _mod in maturin python-adblock userscripts-dependencies/python-stem/python-cryptography:src/rust/Cargo.lock; do
+for _mod in python-adblock; do
   (
     cd ${_mod%:*}
     case $_mod in
@@ -23,15 +23,14 @@ for _mod in maturin python-adblock userscripts-dependencies/python-stem/python-c
 done
 
 # python modules with multiple dependencies and have a requirements.txt file
-for _mod in userscripts-dependencies/python-{beautifulsoup4,pocket-api,pykeepass,readability-lxml,stem/python-cryptography/python-setuptools-rust,tldextract}; do
-  (
-    cd $_mod
-    ${_TOOLSDIR}/pip-updater $(basename $_mod)
-  )
-done
+#for _mod in tests; do
+#  (
+#    cd $_mod
+#    ${_TOOLSDIR}/pip-updater $(basename $_mod)
+#  )
+#done
 
 # make only modules
-for _mod in asciidoc maturin-bin/maturin-bin-{aarch64,x86_64} \
-  userscripts-dependencies/python-stem/python-cryptography/python-setuptools-rust{,/python-{semantic-version,typing-extensions}}; do
-  ${_TOOLSDIR}/fedc-merge-updater $_mod
-done
+#for _mod in asciidoc maturin-bin/maturin-bin-{aarch64,x86_64}; do
+#  ${_TOOLSDIR}/fedc-merge-updater $_mod
+#done
